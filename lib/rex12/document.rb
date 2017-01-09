5655387e087f982a9894e98ddf36612884988814
jhulford <jhulford@vandegriftinc.com>
2017-01-09 09:03:11 -0600

    Adds Document#each_transaction (#1)
    * Remove spec_helper requires from files, move to .rspec instead.
    
    * Allow using IO objects to read EDI data from.
    
    * Add each_transaction method

--- lib/rex12/document.rb
+++ lib/rex12/document.rb
@@ -4,26 +4,29 @@
 # but if you use the block form of the methods, then the subsequent ruby objects
 # are created within the block loops so they can be garbage collected
 module REX12; class Document
+
   # Parse the EDI document from file
   #
   # @return (see #parse)
-  def self.read path
+  def self.read path_or_io
     if block_given?
-      parse(IO.read(path)) {|s| yield s}
+      parse(file_text(path_or_io)) {|s| yield s}
       return nil
     else
-      return parse(IO.read(path))
+      return parse(file_text(path_or_io))
     end
   end
-  # Parse the EDI document from text
+
+  # Parse the EDI document from text or IO object
   #
-  # @return [Enumerator<REX12::Segment>,nil] all segments or nil for block form
+  # @return [Array<REX12::Segment>,nil] all segments or nil for block form
   def self.parse text
     validate_isa(text)
     element_separator = text[3]
     segment_terminator = determine_segment_terminator(text)
     sub_element_separator = text[104]
     r = []
+
     text.split(segment_terminator).each_with_index do |seg_text,pos|
       next if seg_text.length == 0
       seg = REX12::Segment.new(seg_text,element_separator,sub_element_separator,pos)
@@ -33,6 +36,7 @@ module REX12; class Document
         r << seg
       end
     end
+
     if block_given?
       return nil
     else
@@ -40,7 +44,52 @@ module REX12; class Document
     end
   end
 
+  # Parses the EDI document from text or IO object, returning or yielding every transaction from the 
+  # document.
+  #
+  # @return [Array<REX12::Transaction>,nil] all transactions or nil for block form
+  def self.each_transaction text
+    isa = nil
+    current_gs = nil
+    current_segments = []
+    transactions = []
+
+    parse(text) do |segment|
+      segment_type = segment.segment_type
+      case segment_type
+      when "ISA"
+        isa = segment
+      when "GS"
+        current_gs = segment
+      when "IEA", "GE"
+        # Do nothing, we don't care about the trailer segments
+      else
+        current_segments << segment
+
+        # If we found the transaction trailer, it means we can take all the current segments we have and process them
+        if segment_type == "SE"
+          transaction = REX12::Transaction.new(isa, current_gs, current_segments)
+
+          if block_given? 
+            yield transaction
+          else
+            transactions << transaction
+          end
+
+          current_gs = nil
+          current_segments = []
+        end
+      end
+    end
+
+    block_given? ? nil : transactions
+  end
+
   def self.determine_segment_terminator text
+    # Technically, allowing multi-character terminators is not valid EDI, but you 
+    # see it happen ALL the time with EDI that's been hand editted from a client (especially on Windows).
+    # It's a valid enough use-case that we're accounting for it.
+
     # no segement terminator, just CRLF
     return "\r\n" if text[105..106]=="\r\n"
     # no segement terminator, just CR or LF
@@ -50,15 +99,21 @@ module REX12; class Document
     # segment terminator with CRLF
     return text[105..107] if text[106..107]=="\r\n"
     return text[105..106] if ["\r","\n"].include?(text[106]) && text[107]=="G"
-    raise REX12::ParseError, "Cannot determine segment terminator, invalid ISA / GS."
+    raise REX12::ParseError, "Invalid ISA / GS segements. Could not determine segment terminator."
   end
   private_class_method :determine_segment_terminator
 
   def self.validate_isa text
     raise REX12::ParseError, "EDI file must be at least 191 characters to include a valid envelope." unless text.length > 191
     str = text[0..2]
-    raise REX12::ParseError, "First 3 characters must be ISA, they were #{str}" unless str=='ISA'
+    raise REX12::ParseError, "First 3 characters must be ISA. They were '#{str}'." unless str=='ISA'
     return
   end
   private_class_method :validate_isa
+
+  # Allow reading from an object that responds to #read or assume the given param is a string file path and read with IO
+  def self.file_text path_or_io
+    path_or_io.respond_to?(:read) ? path_or_io.read : IO.read(path_or_io)
+  end
+  private_class_method :file_text
 end; end
--- spec/rex12/segment_spec.rb
+++ spec/rex12/segment_spec.rb
@@ -1,5 +1,3 @@
-require "spec_helper"
-
 describe REX12::Segment do
   let :base_text do
     'DTM*ACK*20000415*0830*ED>XYZ'
